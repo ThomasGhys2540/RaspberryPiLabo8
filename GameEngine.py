55ab078d5a4bab1cfc449724e8e8c59915824203
Ailko <ailko.claeys@outlook.com>
2021-05-06 13:38:35 +0200

    Added GameMath imports to GameEngine

--- GameEngine.py
+++ GameEngine.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
-import GameMath
+import Point from GameMath as Point
+import Vector from GameMath as Vector
+import Circle from GameMath as Circle
+import Rectangle from GameMath as Rectangle
